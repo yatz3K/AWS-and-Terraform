367e77989e8b867fa7903200608b5d92802192a4
= <=>
2022-03-06 12:53:57 +0200

    updated user-data.tf file

--- homework3/user-data.tf
+++ homework3/user-data.tf
@@ -3,12 +3,9 @@ my-nginx-userdata = <<USERDATA
  #!/bin/bash
  sudo sudo amazon-linux-extras install -y nginx1
  sudo systemctl start ngnix
- echo "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en'><head><title> Welcome to Grandpa's Whiskey </title></head><body><h1> Welcome to Grandpa's Whiskey </h1></body></html> $(hostname)" | sudo tee /usr/share/nginx/html/index.html
+ echo "<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en'><head><title> Welcome to Grandpa's Whiskey </title></head><body><h1> Welcome to Grandpa's Whiskey $HOSTNAME </h1></body></html>" | sudo tee /usr/share/nginx/html/index.html
  sudo systemctl restart nginx.service
- echo "BUCKET=itzick-opsschool-whiskey
- sudo gzip -c /var/log/nginx/access.log > /tmp/access.log.gz
- aws s3 cp /tmp/access.log.gz s3://$BUCKET/access_`date +%Y%m%dT%H%M%SZ`_$PUBLIC_IP.log.gz --region us-east-1 " | sudo tee /home/ec2-user/logs.sh
- sudo chmod +x /home/ec2-user/logs.sh
- ./logs.sh
+ sudo sh -c 'echo -e "#!/bin/bash \nsudo aws s3 cp /var/log/nginx/access.log  s3://itzick-opsschool-whiskey/logs/access_`date +%Y%m%dT%H%M%SZ`" > /etc/cron.hourly/s3.sh'
+ sudo chmod +x /etc/cron.hourly/s3.sh
 USERDATA
 }
